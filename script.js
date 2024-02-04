88b93f5fcd30bb4fda6a00dda694de37c2ec35cc
adwait111 <suvarnarahane111@gmail,com>
2024-02-05 00:07:52 +0530

    added

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 console.log("Hello")
 // Set your username: git config --global user.name "adwait111"
 // Set your email address: git config --global user.email "suvarnarahane111@gmail,com"
+console.log("bye")
+console.log("bye2")
+console.log("bye3")
